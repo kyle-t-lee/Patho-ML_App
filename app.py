4f97bc24f186c47887dcbf583d7426e1299a8ada
joenguyen99 <joe.khiem.nguyen@gmail.com>
2021-04-11 18:12:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,34 @@ from PIL import Image
 app = Flask(__name__)
 
 # Modelling Task
-model = models.resnet18()
-num_inftr = model.fc.in_features
-model.fc = nn.Linear(num_inftr, 4)
-model.load_state_dict(torch.load('./fix_resnet18.pth'))
+#model = models.resnet18()
+#num_inftr = model.fc.in_features
+#model.fc = nn.Linear(num_inftr, 4)
+#model.load_state_dict(torch.load('./fix_resnet18.pth'))
+#model.eval()
+model = models.vgg16(pretrained=True)
+# Change to model state pict path on your computer
+model_state_dict_path = r"C:\Users\joekh\OneDrive\Desktop\Senior Design Code\model_2020_2021\model_02252021\vgg_subclass_model_state_dict_02252021.pt"
+model.load_state_dict(torch.load(model_state_dict_path,map_location=torch.device('cpu')))
 model.eval()
 
-class_names = ['Adenosis', 'Fibroadenoma', 'Tubular Adenoma', 'Phyllodes Tumor', 'Lobular Carcinoma', 'Mucinous Carcinoma', 'Papillary Carcinoma', 'Ductal Carcinoma']
+class_names = ["Adenosis","Ductal Carcinoma", "Fibroadenoma", "Lobular Carcinoma","Mucinous Carcinoma","Papillary Carcinoma","Phyllodes Tumor","Tubular Adenoma"]
+
+def image_loader(loader, image_name):
+    image = Image.open(image_name)
+    image = loader(image).float()
+    image = torch.tensor(image, requires_grad=True)
+    image = image.unsqueeze(0)
+    return image
+
+imsize = 256
+loader = transforms.Compose([
+            transforms.RandomSizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
 
 def transform_image(image_bytes):
 	my_transforms = transforms.Compose([
@@ -32,15 +53,20 @@ def transform_image(image_bytes):
 
 def get_prediction(image_bytes):
 	tensor = transform_image(image_bytes=image_bytes)
+	#tensor = image_loader(loader, image_bytes)
 	outputs = model.forward(tensor)
 	_, prediction = torch.max(outputs, 1)
 	return class_names[prediction]
 
 diseases = {
-	"Healthy": "",
-	"Apple Scab": "Apple scab is a common disease of plants in the rose family (Rosaceae) that is caused by the ascomycete fungus Venturia inaequalis. While this disease affects several plant genera, including Sorbus, Cotoneaster, and Pyrus, it is most commonly associated with the infection of Malus trees, including species of flowering crabapple, as well as cultivated apple.",
-	"Cedar Apple Rust": "Gymnosporangium juniperi-virginianae is a plant pathogen that causes cedar-apple rust. In virtually any location where apples or crabapples (Malus) and Eastern red-cedar (Juniperus virginiana) coexist, cedar apple rust can be a destructive or disfiguring disease on both the apples and cedars. Quince and hawthorn are the most common host and many species of juniper can substitute for the eastern red cedars.",
-	"Black Rot": "Black rot is a fungus disease that can cause serious losses in apple orchards, especially in warm, humid areas. The black rot fungus covers a wide geographical range and can infect many hosts other than apple. The role these hosts play in the spread and development of the disease is not known. Three forms of the disease can occur: a leaf spot known as frogeye leaf spot, a fruit rot, and a limb canker. Severe leaf spotting can result in defoliation that weakens the tree, infected fruit become unmarketable, and limb cankers can girdle and eventually kill entire branches."
+	"Adenosis": "",
+	"Ductal Carcinoma": "Apple scab is a common disease of plants in the rose family (Rosaceae) that is caused by the ascomycete fungus Venturia inaequalis. While this disease affects several plant genera, including Sorbus, Cotoneaster, and Pyrus, it is most commonly associated with the infection of Malus trees, including species of flowering crabapple, as well as cultivated apple.",
+	"Fibroadenoma": "Gymnosporangium juniperi-virginianae is a plant pathogen that causes cedar-apple rust. In virtually any location where apples or crabapples (Malus) and Eastern red-cedar (Juniperus virginiana) coexist, cedar apple rust can be a destructive or disfiguring disease on both the apples and cedars. Quince and hawthorn are the most common host and many species of juniper can substitute for the eastern red cedars.",
+	"Lobular Carcinoma": "Black rot is a fungus disease that can cause serious losses in apple orchards, especially in warm, humid areas. The black rot fungus covers a wide geographical range and can infect many hosts other than apple. The role these hosts play in the spread and development of the disease is not known. Three forms of the disease can occur: a leaf spot known as frogeye leaf spot, a fruit rot, and a limb canker. Severe leaf spotting can result in defoliation that weakens the tree, infected fruit become unmarketable, and limb cankers can girdle and eventually kill entire branches.",
+	"Mucinous Carcinoma":"",
+	"Papillary Carcinoma":"",
+	"Phyllodes Tumor":"",
+	"Tubular Adenoma":"",
 }
 
 # Treat the web process
